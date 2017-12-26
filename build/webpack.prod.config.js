caf2b4502e4364c6831c19c4f05c69824c4a6eee
刘文龙 <1@gitor.org>
2017-12-26 09:55:20 +0800

    修改build url

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -16,7 +16,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'https://iview.github.io/iview-admin/dist/',
+        publicPath: 'https://iview.17open.org/cc-admin/dist/',
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
