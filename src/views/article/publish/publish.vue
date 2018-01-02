f0039aa7d4343b1dfef9b98696a66af369ada6dc
刘文龙 <1@gitor.org>
2018-01-02 23:18:18 +0800

    li

--- src/views/article/publish/publish.vue
+++ src/views/article/publish/publish.vue
@@ -10,6 +10,16 @@
             </Col>
         </Row>
         <Row class="margin-top-20">
+            <Col span="4" offset="2">
+            <Select v-model="model1" style="width:200px" placeholder="请选择类型……" filterable clearable>
+                <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+            </Col>
+            <Col span="15" offset="1">
+                <Input v-model="article_title" placeholder="请选择标签"></Input>
+            </Col>
+        </Row>
+        <Row class="margin-top-20">
             <Col span="20" offset="2">
             <div class="markdown-con">
                 <Card>
@@ -36,7 +46,34 @@ export default {
     name: 'markdown-editor',
     data () {
         return {
-            article_title: ''
+            article_title: '',
+            cityList: [
+                {
+                    value: 'New York',
+                    label: '散文'
+                },
+                {
+                    value: 'London',
+                    label: '随笔'
+                },
+                {
+                    value: 'Sydney',
+                    label: '小说'
+                },
+                {
+                    value: 'Ottawa',
+                    label: '吹牛'
+                },
+                {
+                    value: 'Paris',
+                    label: '测试'
+                },
+                {
+                    value: 'Canberra',
+                    label: '不知道'
+                }
+            ],
+            model1: ''
         };
     },
     methods : {
@@ -47,8 +84,7 @@ export default {
             alert('保存成功');
         }
     },
-    mounted ()
-    {
+    mounted () {
         new SimpleMDE({
             element: document.getElementById('iview_admin_markdown_editor'),
             toolbar: ['bold', 'italic', 'strikethrough', 'heading', 'heading-smaller', 'heading-bigger', 'heading-1', 'heading-2', 'heading-3', '|', 'code', 'quote', 'unordered-list', 'clean-block', '|', 'link', 'image', 'table', 'horizontal-rule', '|', 'preview', 'guide']
