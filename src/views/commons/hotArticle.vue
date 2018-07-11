6d2a956e4217d63f411ee5f20575cdc85e0f869b
gitor <1@gitor.org>
2018-07-11 23:00:31 +0800

    ffff:s:

---
+++ src/views/commons/hotArticle.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="margin-top-10">
+        <Card>
+            <p slot="title">
+                <Icon type="fireball"></Icon>
+                热门文章
+            </p>
+            <a href="#" slot="extra" @click.prevent="">
+                <Icon type="more"></Icon>
+            </a>
+            <div class="preview-placeholderCon">
+                <p v-for="item in hotartical"><a>{{item.name}}</a></p>
+            </div>
+        </Card>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                hotartical: [
+                    {'id': '1', 'name': '最新文章……'},
+                    {'id': '1', 'name': '又一篇新文章！！！'},
+                    {'id': '1', 'name': '测试'},
+                    {'id': '1', 'name': '测试'}
+                ]
+            };
+        }
+    }
+</script>
+
