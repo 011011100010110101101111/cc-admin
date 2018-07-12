c33cbb1eb1c36233ec63d157aa8ec07fe8647de3
gitor <1@gitor.org>
2018-07-12 11:04:47 +0800

    评论显示

--- src/views/article/articleContent.vue
+++ src/views/article/articleContent.vue
@@ -11,13 +11,17 @@
                 <Tag v-for="item in tagsList" type="border" color="blue" :key="item">{{ item }}</Tag>
             </div>
             <div class="preview-content-con" v-html="content"></div>
-            <div class="preview-classifition-con">
+            <div>
                 <b class="preview-tip">
-                    <Icon type="navicon-round"></Icon>&nbsp;目录：</b>
-                <a class="preview-classifition-item" v-for="item in classificationSelected" :key="item">
-                    <Icon type="android-folder-open"></Icon>
-                    {{ item }}
-                </a>
+                    <Icon type="navicon-round"></Icon>&nbsp;推荐阅读：
+                </b>
+            </div>
+            <div>
+                <ul style="margin-top: 8px">
+                    <li v-for="item in classificationSelected" :key="item">
+                        <a style="font-size: 8px">{{item}}</a>
+                    </li>
+                </ul>
             </div>
             <div style="text-align: right">
                 <ButtonGroup>
@@ -51,7 +55,7 @@
             return {
                 articleTitle: '这是一篇测试文章',
                 tagsList: ['测试', 'Java', '蛋疼'],
-                classificationSelected: ['你','我'],
+                classificationSelected: ['教你如何进行单元测试。','持续集成实战集锦'],
                 content: '2015年中，杜某跟他的同伙在电商平台通过虚假交易的方式为他人套现，以收取一定比例手续费的方式牟利。经公安机关侦查，在2015年11月10日至2015年11月13日期间，杜某等人利用多家网上店铺，在全国范围内串通多名电商用户虚构交易共计2500余笔，从“花呗”套现共计470余万元，从中收取手续费共计40余万元。',
                 publishTime: '2018年01月01日 23:45:00',
                 comment: '',
