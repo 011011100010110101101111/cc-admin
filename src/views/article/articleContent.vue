bac189cea3186de7dcc5129bb35aa9a0b20a7e40
gitor <1@gitor.org>
2018-07-12 09:07:08 +0800

    点赞样式

--- src/views/article/articleContent.vue
+++ src/views/article/articleContent.vue
@@ -19,6 +19,21 @@
                     {{ item }}
                 </a>
             </div>
+            <div style="text-align: right">
+                <ButtonGroup>
+                    <Badge count="10">
+                        <Button @click="click1" type="text">
+                            <Icon type="happy-outline" size="20" color="orange"></Icon>
+                        </Button>
+                    </Badge>
+                    <Button @click="click1" type="text">
+                        <Icon type="sad-outline" size="20"></Icon>
+                    </Button>
+                </ButtonGroup>
+            </div>
+            <div>
+                <i-input type = "textarea"></i-input>
+            </div>
         </Card>
     </div>
 </template>
@@ -32,6 +47,11 @@
                 content: '2015年中，杜某跟他的同伙在电商平台通过虚假交易的方式为他人套现，以收取一定比例手续费的方式牟利。经公安机关侦查，在2015年11月10日至2015年11月13日期间，杜某等人利用多家网上店铺，在全国范围内串通多名电商用户虚构交易共计2500余笔，从“花呗”套现共计470余万元，从中收取手续费共计40余万元。',
                 publishTime: '2018年01月01日 23:45:00'
             };
+        },
+        methods: {
+            click1() {
+                alert('点击啊1');
+            }
         }
     };
 </script>
