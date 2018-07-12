2b72ecf698b55b04479ce9bb509455073a63ffe8
gitor <1@gitor.org>
2018-07-12 09:18:28 +0800

    文章内容页面调整

--- src/views/article/articleContent.vue
+++ src/views/article/articleContent.vue
@@ -31,8 +31,9 @@
                     </Button>
                 </ButtonGroup>
             </div>
-            <div>
-                <i-input type = "textarea"></i-input>
+            <div style="text-align: right">
+                <i-input type = "textarea" placeholder="请输入评论内容……" v-model="comment"></i-input>
+                <Button style="margin-top: 8px">提交</Button>
             </div>
         </Card>
     </div>
@@ -43,9 +44,10 @@
             return {
                 articleTitle: '这是一篇测试文章',
                 tagsList: ['测试', 'Java', '蛋疼'],
-                classificationSelected: [],
+                classificationSelected: ['你','我'],
                 content: '2015年中，杜某跟他的同伙在电商平台通过虚假交易的方式为他人套现，以收取一定比例手续费的方式牟利。经公安机关侦查，在2015年11月10日至2015年11月13日期间，杜某等人利用多家网上店铺，在全国范围内串通多名电商用户虚构交易共计2500余笔，从“花呗”套现共计470余万元，从中收取手续费共计40余万元。',
-                publishTime: '2018年01月01日 23:45:00'
+                publishTime: '2018年01月01日 23:45:00',
+                comment: ''
             };
         },
         methods: {
