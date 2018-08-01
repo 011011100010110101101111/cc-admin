149272f7b147bbcfcd04b559ee5cc720e2e5d8ab
gitor <1@gitor.org>
2018-08-01 22:58:53 +0800

    aaaa

--- src/views/user/userHome.vue
+++ src/views/user/userHome.vue
@@ -1,15 +1,18 @@
-<style lang="less">
+<style lang="less" scoped>
     @import "userHome.less";
+</style>
+<style>
+    @import '~@/styles/common.less';
 </style>
 <template>
     <div class="userHome-index">
         <Card>
             <userHomeComponent></userHomeComponent>
-            <Row>
-                <i-col span="12">
+            <Row class="margin-top-10">
+                <i-col span="6" class="avatar">
                     <img src="https://avatars2.githubusercontent.com/u/31715318?s=460&v=4"/>
                 </i-col>
-                <i-col span="12">
+                <i-col span="18" class="padding-left-10">
                     <p>姓名：刘文龙</p>
                 </i-col>
             </Row>
